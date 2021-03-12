c6c0e80f8655208f7d6e7937dc52fbd929184e04
Jon Andre Briones <jbriones1@my.bcit.ca>
2021-03-12 14:08:36 -0800

    Cleaned up src

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Tony you're the best
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Tony to the moon
-        </a>
-        <p>good bye yall</p>
-      </header>
+			<h1>Tidy</h1>
     </div>
   );
 }
