48e4e02150bbfdf0c2680a15e39a542d4249adea
Scott Lai <tuonglai3602@gmail.com>
2021-03-13 11:01:43 -0800

    Removed logos (sorry).

--- src/App.js
+++ src/App.js
@@ -9,26 +9,22 @@ import {
   Grid,
   theme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
+      <Box textAlign='center' fontSize='xl'>
+        <Grid minH='100vh' p={3}>
           <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
             <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
+              Edit <Code fontSize='xl'>src/App.js</Code> and save to reload.
             </Text>
             <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
+              color='teal.500'
+              href='https://chakra-ui.com'
+              fontSize='2xl'
+              target='_blank'
+              rel='noopener noreferrer'
             >
               Learn Chakra
             </Link>
@@ -40,4 +36,3 @@ function App() {
 }
 
 export default App;
-
