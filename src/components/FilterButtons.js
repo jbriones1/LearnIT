7d25a6f2b6fff3741e75ecb13e16601f9d88638a
Scott Lai <tuonglai3602@gmail.com>
2021-03-13 22:43:29 -0800

    Added logo.

--- src/components/FilterButtons.js
+++ src/components/FilterButtons.js
@@ -1,67 +1,65 @@
-import React, {useEffect} from 'react';
-import { Button, SimpleGrid, Text } from "@chakra-ui/react";
+import React, { useEffect } from 'react';
+import { Button, SimpleGrid, Text } from '@chakra-ui/react';
 
-
-
-const FilterButtons = props => {
-  const {colNum, handleClick, techFilter} = props;
+const FilterButtons = (props) => {
+  const { colNum, handleClick, techFilter } = props;
 
   const languages = [
-    {name: 'Rust', icon: 'devicon-rust-plain'},
-    {name: 'Javascript', icon:'devicon-javascript-plain c'},
-    {name: 'Haskell', icon: 'devicon-haskell-plain'},
-    {name: 'Java', icon: 'devicon-java-plain'},
-    {name: 'Objective-c', icon: 'devicon-objectivec-plain'},
-    {name: 'Scala', icon: 'devicon-scala-plain'},
-    {name: 'Golang', icon: 'devicon-go-plain'},
-    {name: 'Kotlin', icon: 'devicon-kotlin-plain'},
-    {name: 'Ruby', icon: 'devicon-ruby-plain'},
-    {name: 'Python', icon: 'devicon-python-plain'},
-    {name: 'C++', icon: 'devicon-cplusplus-plain'},
-    {name: 'C#', icon: 'devicon-csharp-plain'},
-    {name: 'Php', icon: 'devicon-php-plain'},
-    {name: 'F#', icon: 'devicon-fsharp-plain'},
-    {name: 'Ocaml', icon: 'devicon-ocaml-plain'},
-    {name: 'Typescript', icon: 'devicon-typescript-plain'},
-    {name: 'Elixir', icon: 'devicon-elixir-plain'},
-    {name: 'Swift', icon: 'devicon-swift-plain'},
-    {name: 'C', icon: 'devicon-c-plain'}
+    { name: 'Rust', icon: 'devicon-rust-plain' },
+    { name: 'Javascript', icon: 'devicon-javascript-plain c' },
+    { name: 'Haskell', icon: 'devicon-haskell-plain' },
+    { name: 'Java', icon: 'devicon-java-plain' },
+    { name: 'Objective-c', icon: 'devicon-objectivec-plain' },
+    { name: 'Scala', icon: 'devicon-scala-plain' },
+    { name: 'Golang', icon: 'devicon-go-plain' },
+    { name: 'Kotlin', icon: 'devicon-kotlin-plain' },
+    { name: 'Ruby', icon: 'devicon-ruby-plain' },
+    { name: 'Python', icon: 'devicon-python-plain' },
+    { name: 'C++', icon: 'devicon-cplusplus-plain' },
+    { name: 'C#', icon: 'devicon-csharp-plain' },
+    { name: 'Php', icon: 'devicon-php-plain' },
+    { name: 'F#', icon: 'devicon-fsharp-plain' },
+    { name: 'Ocaml', icon: 'devicon-ocaml-plain' },
+    { name: 'Typescript', icon: 'devicon-typescript-plain' },
+    { name: 'Elixir', icon: 'devicon-elixir-plain' },
+    { name: 'Swift', icon: 'devicon-swift-plain' },
+    { name: 'C', icon: 'devicon-c-plain' },
   ];
 
   const renderButtons = () => {
-    console.log("Render")
+    console.log('Render');
     return languages.map((language, index) => {
       return (
-        <Button 
+        <Button
           key={index}
-          bg={techFilter.includes(language.name.toLowerCase()) ? '#1B213B'  : '#FFF'}
+          bg={
+            techFilter.includes(language.name.toLowerCase())
+              ? '#1B213B'
+              : '#FFF'
+          }
           color='#A0AEC0'
           leftIcon={<i className={language.icon} />}
           width='120px'
           onClick={() => handleClick(language.name)}
-          >
+        >
           {language.name}
         </Button>
-      )
+      );
     });
-  }
+  };
 
-  let buttonList = renderButtons();
-
+  const buttonList = renderButtons();
 
   return (
     <>
-    <Text
-      color="white"
-      align="left"
-      fontSize="lg"
-      mb="5"
-      >Filter the languages:</Text>
-    <SimpleGrid columns={colNum} spacingX="10px" spacingY="10px">
-      {buttonList}
-    </SimpleGrid>
+      <Text color='white' align='left' fontSize='lg' mb='5'>
+        Filter the languages:
+      </Text>
+      <SimpleGrid columns={colNum} spacingX='10px' spacingY='10px'>
+        {buttonList}
+      </SimpleGrid>
     </>
   );
-}
+};
 
 export default FilterButtons;
