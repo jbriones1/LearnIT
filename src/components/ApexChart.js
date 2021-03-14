87a3a0619d559aa2af63bc8f4636b30b91bc4f6d
Jon Andre Briones <jbriones1@my.bcit.ca>
2021-03-13 17:59:15 -0800

    Completed two graphs

--- src/components/ApexChart.js
+++ src/components/ApexChart.js
@@ -10,60 +10,133 @@ const ApexChart = props => {
     typeChart
   } = props;
 
-  const languages = states["FL"].languages;
+  const state = '' || 'FL'
+  const languages = states[state].languages.filter(lang => lang.y > 0).sort((a, b) => b.y - a.y).slice(0, 10);
+  const technologies = states[state].technologies.filter(lang => lang.y > 0).sort((a, b) => b.y - a.y).slice(0, 10);
 
-  // you need to change data field and categories
-  const barGraph = {
+  const languagesGraph = {
     series: [{
-      data: languages.sort((a, b) => b.y - a.y)
+      data: languages
     }],
     options: {
       chart: {
         type: 'bar',
+        background: '#262D47'
       },
       plotOptions: {
         bar: {
           horizontal: false,
         }
       },
+      theme: {
+        mode: 'dark',
+        palette: 'palette7',
+        monochrome: {
+          enabled: false,
+          color: '#262D47',
+          shadeTo: 'dark',
+          shadeIntensity: 0.65
+        },
+      },
+      fill: {
+        type: 'gradient',
+        gradient: {
+          shade: 'light',
+          gradientToColors: ['#FDD835'],
+          shadeIntensity: 1,
+          type: 'vertical',
+          opacityFrom: 1,
+          opacityTo: 1,
+          stops: [0, 100, 100, 100]
+        },
+      },
+      colors: ['#F44336', '#E91E63', '#9C27B0'],
       dataLabels: {
         enabled: false
       },
       xaxis: {
         type: 'category'
+      },
+      yaxis: {
+        title: {
+          text: 'Job postings',
+          style: {
+            fontSize: '12px'
+          }
+        }
       }
     },
   };
 
-  return ( <
-    Chart options = {
-      barGraph.options
-    }
-    series = {
-      barGraph.series
-    }
-    type = {
-      typeChart
-    }
-    width = "500" /
-    >
+  const technologyGraph = {
+    series: [{
+      data: technologies
+    }],
+    options: {
+      chart: {
+        type: 'bar',
+        background: '#262D47'
+      },
+      plotOptions: {
+        bar: {
+          horizontal: false,
+        }
+      },
+      theme: {
+        mode: 'dark',
+        palette: 'palette7',
+        monochrome: {
+          enabled: false,
+          color: '#262D47',
+          shadeTo: 'dark',
+          shadeIntensity: 0.65
+        },
+      },
+      fill: {
+        type: 'gradient',
+        gradient: {
+          shade: 'light',
+          gradientToColors: ['#FDD835'],
+          shadeIntensity: 1,
+          type: 'vertical',
+          opacityFrom: 1,
+          opacityTo: 1,
+          stops: [0, 100, 100, 100]
+        },
+      },
+      colors: ['#9C27B0'],
+      dataLabels: {
+        enabled: false
+      },
+      xaxis: {
+        type: 'category'
+      },
+      yaxis: {
+        title: {
+          text: 'Job postings',
+          style: {
+            fontSize: '12px'
+          }
+        }
+      }
+    },
+  };
+
+  return (<div>
+    <Chart
+      options={languagesGraph.options}
+      series={languagesGraph.series}
+      type={typeChart}
+      width="500"
+    />
+    <Chart
+      options={technologyGraph.options}
+      series={technologyGraph.series}
+      type={typeChart}
+      width="500"
+    />
+  </div>
   )
 }
 
 export default ApexChart;
-
-// const jsonToArray = (object) => {
-//   let result = [];
-
-//   Object.keys(object).forEach(key => {
-
-//     let temp = {
-//       x: key,
-//       y: object[key]
-//     }
-
-//     result.push(temp);
-//   });
-
-//   return result;
-// }
